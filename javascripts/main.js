279a25711560dedb90cff3158c5273158942c09c
Hane Summer <haneyuzuru@yahoo.com>
2016-05-17 21:35:18 +0800

    nodejs

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+// console.log('This would be the main JS file.');
+//
+// var counter1 = require('./util/counter.js');
+// var counter2 = require('./util/counter');
+//
+// console.log(counter1.count());
+// console.log(counter2.count());
+// console.log(counter1.count());
+
+
+
---
+++ javascripts/util/counter.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Yita on 16/5/17.
+ */
+var i = 0;
+
+function count() {
+    return ++i;
+}
+
+exports.count = count;
